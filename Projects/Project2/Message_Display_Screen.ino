7e052679d2ba899e27c0dbf35e343834fdc55663
Sancharini Dey <67041010+sancharinee@users.noreply.github.com>
2021-07-19 12:09:49 +0530

    Add files via upload

---
+++ Projects/Project2/Message_Display_Screen.ino
@@ -0,0 +1,18 @@
+#include<LiquidCrystal.h> //Library for LCD
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);// LCD pins at which it is attached to the Arduino
+
+void setup() //method used to run the source for the one time onlys
+{
+  lcd.begin(16, 2);//LCD order i.e. 16 columns & 2 rows
+  lcd.print("Project2");//prints on LCD
+  lcd.setCursor(0,1);//setting cursor on LCD
+  lcd.print("Messaage Display Screen");//prints on LCD
+  delay(1000);//delay of 1 sec
+}
+void loop() //method to run the source code repeatedly 
+{
+ lcd.noDisplay();//turn off the display of LCD
+ delay(250);//delay to 0.25 seconds
+ lcd.display();//turning on the LCD display
+ delay(250); //delay of 0.25 seconds again
+}
